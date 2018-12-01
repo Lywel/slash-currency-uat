eb5db2e6c4e400877035727def7fe096f9f05a22
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2018-12-01 13:10:03 +0100

    Updates

---
+++ precache-manifest.72591666b45d65af999c11146b85e7f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60ee3a9a7e942203e375",
+    "url": "/slash-currency-uat/static/css/main.6c0b14ed.chunk.css"
+  },
+  {
+    "revision": "60ee3a9a7e942203e375",
+    "url": "/slash-currency-uat/static/js/main.60ee3a9a.chunk.js"
+  },
+  {
+    "revision": "936f326906058fcefbb1",
+    "url": "/slash-currency-uat/static/css/1.b67b27c6.chunk.css"
+  },
+  {
+    "revision": "936f326906058fcefbb1",
+    "url": "/slash-currency-uat/static/js/1.936f3269.chunk.js"
+  },
+  {
+    "revision": "3758b002e82725cc1bae",
+    "url": "/slash-currency-uat/static/js/runtime~main.3758b002.js"
+  },
+  {
+    "revision": "d30e01587597e27d84fe69dc601a3503",
+    "url": "/slash-currency-uat/index.html"
+  }
+];
