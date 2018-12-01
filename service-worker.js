7d4054fc1c2cf421ca70646e033c8a98de130bd9
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2018-12-01 13:24:39 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/slash-currency-uat/precache-manifest.72591666b45d65af999c11146b85e7f5.js"
+  "/precache-manifest.7b7ae3b3baa49067607a4e3262227bbc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/slash-currency-uat/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
