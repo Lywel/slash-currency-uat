e30ede3fb4344b164141de692f0e8d10bbbda24d
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2018-12-01 12:09:00 +0100

    Updates

---
+++ precache-manifest.7b7ae3b3baa49067607a4e3262227bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9f0ab392c13480f3874",
+    "url": "/static/css/main.6c0b14ed.chunk.css"
+  },
+  {
+    "revision": "c9f0ab392c13480f3874",
+    "url": "/static/js/main.c9f0ab39.chunk.js"
+  },
+  {
+    "revision": "936f326906058fcefbb1",
+    "url": "/static/css/1.b67b27c6.chunk.css"
+  },
+  {
+    "revision": "936f326906058fcefbb1",
+    "url": "/static/js/1.936f3269.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "729d725d180d3171cb6308625fb71035",
+    "url": "/index.html"
+  }
+];
